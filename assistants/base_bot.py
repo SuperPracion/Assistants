84433fb1612e7ec2b0e402a6c6a113d0eb71162a
Akhmetov Timur <superpracion2@gmai.com>
2024-10-15 12:23:08 +0300

    Добавлено асинхронное начало работы метода start, родительского класса base_bot

--- assistants/base_bot.py
+++ assistants/base_bot.py
@@ -1,3 +1,5 @@
+from multiprocessing.util import sub_warning
+
 from aiogram import Bot, Dispatcher
 from abc import ABC, abstractmethod
 
@@ -7,10 +9,10 @@ class BaseBot(ABC):
         self.bot = Bot(token=token)
         self.dp = Dispatcher()
 
-    def start(self):
+    async def start(self):
         """Метод для запуска работы"""
         self.register_handlers()
-        self.dp.start_polling(self.bot)
+        await self.dp.start_polling(self.bot)
 
     @abstractmethod
     def register_handlers(self):
