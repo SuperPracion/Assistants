41b7a594821c753cba51e2271eb06d0e2ccf1b23
Akhmetov Timur <superpracion2@gmai.com>
2024-10-15 14:47:11 +0300

    Описан WordsGamer класс

---
+++ assistants/english_words_gamer/english_words_gamer_bot.py
@@ -0,0 +1,56 @@
+import asyncio
+import settings
+from aiogram import types
+from tinydb import TinyDB, Query
+from random import randint, shuffle
+from assistants.base_bot import BaseBot
+from aiogram.filters.command import Command
+
+
+class MyBot(BaseBot):
+    def __init__(self, token: str):
+        super().__init__(token)
+
+    def register_handlers(self):
+        # TODO добавить обработку только для админов и конкретного чата
+        @self.dp.message(Command('start'))
+        async def start_command(message: types.Message):
+            while True:
+                with (TinyDB('words.json', encoding='utf-8') as words):
+                    rnd_words = list()
+                    for _ in range(4):
+                        rnd_words.append(words.search(Query().id == randint(0, len(words)))[0])
+
+                correct_word = rnd_words[0]
+                shuffle(rnd_words)
+                send_poll = await message.answer_poll(
+                    question=f'🤔Как же переводится слово {correct_word['word']} {correct_word['abbreviation']}?',
+                    options=[f'{word['translation']}' for word in rnd_words],
+                    type='quiz',
+                    correct_option_id=words.index(correct_word),
+                    is_anonymous=False
+                )
+
+                with (TinyDB('polls.json', encoding='utf-8') as polls):
+                    polls.insert({'poll_id': send_poll.poll.id,
+                                  'word_id': correct_word['id'],
+                                  'correct_option_id': send_poll.poll.correct_option_id})
+                await asyncio.sleep(settings.poll_interval)
+
+        @self.dp.message(Command('stop'))
+        async def help_command(message: types.Message):
+            await self.shutdown()
+
+        @self.dp.poll_answer()
+        async def poll_answer(answer: types.PollAnswer):
+            with (
+                TinyDB('polls.json', encoding='utf-8') as polls,
+                TinyDB('words.json', encoding='utf-8') as words
+            ):
+                poll = polls.search(Query().poll_id == answer.poll_id)[0]
+                word = words.search(Query().id == poll['word_id'])[0]
+
+                if poll['correct_option_id'] == answer.option_ids[0]:
+                    word['correct_answers'] += 1
+                word['total_answers'] += 1
+                words.upsert(word, Query().id == poll['word_id'])
