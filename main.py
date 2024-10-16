188cb3391ea5f94317d6f681f5b0891a04cc15b0
Akhmetov Timur <superpracion2@gmai.com>
2024-10-16 14:09:54 +0300

    minor main

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import asyncio
 from assistants.reminiscent_about_classes.reminiscent_about_classes_bot import ReminiscentAboutClassesBot
 from assistants.english_words_gamer.english_words_gamer_bot import EnglishWordsGamerBot
 
+
 async def main():
     remin_bot = ReminiscentAboutClassesBot()
     await remin_bot.start()
 
     # english_bot = EnglishWordsGamerBot()
     # await english_bot.start()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
