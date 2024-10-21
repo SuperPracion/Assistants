8ea56b295aba831caf8e2cbadb8d67887448a410
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-21 17:08:25 +0300

    Добавлен шаблон класса Poll для работы с опросами

---
+++ assistants/english_words_gamer/polls.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Polls:
+    id: int
+    word_id: int
+    correct_option_id: int
+    chat_id: int
+    poll_id: int
+    create_moment: str
+    update_moment: str
+
+    def __post_init__(self):
+        # TOOD Вынест в родителя
+        self.create_moment = datetime.strptime(self.create_moment, '%Y-%m-%d %H:%M:%S')
+        self.update_moment = datetime.strptime(self.update_moment, '%Y-%m-%d %H:%M:%S')
