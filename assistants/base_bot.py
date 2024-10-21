5c9e1da2ad40d845a56eccfed7b3cdf47ba0d19e
SuperPracion <superpracion2@gmail.com>
2024-10-21 22:32:25 +0300

    Добавлены аттрибуты для работы с sqlite бд

--- assistants/base_bot.py
+++ assistants/base_bot.py
@@ -1,11 +1,15 @@
+from sqlite3 import connect
 from aiogram import Bot, Dispatcher
 from abc import ABC, abstractmethod
 
 class BaseBot(ABC):
-    def __init__(self, token: str):
+    def __init__(self, token, db_name):
         self.token = token
-        self.bot = Bot(token=token)
+        self.bot = Bot(self.token)
         self.dp = Dispatcher()
+        self.db_name = db_name
+        self.connect = connect(self.db_name)
+        self.cursor = self.connect.cursor()
 
     async def start(self, *args, **kwargs):
         """Метод для запуска работы"""
@@ -20,3 +24,10 @@ class BaseBot(ABC):
     async def shutdown(self):
         """Метод остановки"""
         await self.bot.close()
+
+    async def auto_db_commit(func):
+        async def wrapper(self, *args, **kwargs):
+            res = func(self, *args, **kwargs)
+            self.connect.commit()
+            return res
+        return wrapper
