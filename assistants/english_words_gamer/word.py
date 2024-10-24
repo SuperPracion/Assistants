b71647588c7b8053a5825acb8a27e62041d19796
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-24 18:28:40 +0300

    Методы вынесены в базовый класс
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- assistants/english_words_gamer/word.py
+++ assistants/english_words_gamer/word.py
@@ -1,20 +1,11 @@
-from datetime import datetime
 from dataclasses import dataclass
-
+from assistants.base_record import BaseRecord
 
 @dataclass
-class Word:
-    id: int
+class Word(BaseRecord):
     word: str
     abbreviation: str
     translation: str
     total_answers: int
     correct_answers: int
     part_of_speech: str
-    create_moment: str
-    update_moment: str
-
-    def __post_init__(self):
-        # TODO Вынести в родителя
-        self.create_moment = datetime.strptime(self.create_moment, '%Y-%m-%d %H:%M:%S')
-        self.update_moment = datetime.strptime(self.update_moment, '%Y-%m-%d %H:%M:%S')
