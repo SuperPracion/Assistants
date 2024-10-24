c0bda8808cef63fa3e3349a1f50fd7f80d53216a
Timur A. <59648797+superpracion@users.noreply.github.com>
2024-10-24 18:28:54 +0300

    Методы вынесены в базовый класс
    Signed-off-by: SuperPracion <superpracion2@gmail.com>

--- assistants/english_words_gamer/polls.py
+++ assistants/english_words_gamer/polls.py
@@ -1,17 +1,9 @@
-from datetime import datetime
 from dataclasses import dataclass
+from assistants.base_record import BaseRecord
 
 @dataclass
-class Poll:
-    id: int
+class Poll(BaseRecord):
     word_id: int
     correct_option_id: int
     chat_id: int
     poll_id: int
-    create_moment: str
-    update_moment: str
-
-    def __post_init__(self):
-        # TODO Вынести в родителя
-        self.create_moment = datetime.strptime(self.create_moment, '%Y-%m-%d %H:%M:%S')
-        self.update_moment = datetime.strptime(self.update_moment, '%Y-%m-%d %H:%M:%S')
